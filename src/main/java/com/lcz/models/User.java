6c98ee50339fa4f88e87279aba06229ea5550a31
344544399@qq.com <344544399@qq.com>
2023-02-03 17:22:34 +0800

    xml sql

---
+++ src/main/java/com/lcz/models/User.java
@@ -0,0 +1,46 @@
+package com.lcz.models;
+
+public class User {
+
+  private long id;
+  private String name;
+  private long age;
+  private String email;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public long getAge() {
+    return age;
+  }
+
+  public void setAge(long age) {
+    this.age = age;
+  }
+
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+}
